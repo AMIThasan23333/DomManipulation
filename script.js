cdbc140d6ff445a631faabcfa50ac84a09c99ce6
My Name <cpgsom819@gmail.com>
2023-02-12 02:19:47 +0800

    var let const

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 
 
 document.getElementById('btn').addEventListener('click', function( ) {
-
-
     const friends = document.getElementsByClassName('friend');
-
     for( const friend of friends){
-
-
         friend.style.backgroundColor = 'red';
     }
 })
+
+
